ba7a0dd99bccf823addd76f0d72ed5a5358adf18
Piscuc Dinu <dinu@piscuc.eu>
2015-05-20 08:12:25 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aerodrom
+//
+//  Created by mac-mac on 5/20/15.
+//  Copyright (c) 2015 mac-mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
